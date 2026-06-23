358bb389ffea3914ea4a1e95d983204de1f43e9f
Bryan Pérez <32740906+brayananx@users.noreply.github.com>
2026-06-23 17:08:40 -0600

    Perfiles de usuario funcionando

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -13,6 +13,10 @@ export class UsersController {
   @Get()
   findAll() {
     return this.usersService.findAll();
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
   }
 
   @Patch('assign-section')
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -5,13 +5,43 @@ import { PrismaService } from '../prisma/prisma.service';
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  create(data: { name: string; email: string; role?: string }) {
-    return this.prisma.user.create({ data });
-  }
+  create(data: {
+  name: string;
+  email: string;
+  identityNumber: string;
+  role?: string;
+  birthDate?: string;
+  joinDate?: string;
+}) {
+  return this.prisma.user.create({
+    data: {
+      name: data.name,
+      email: data.email,
+      identityNumber: data.identityNumber,
+      role: data.role || 'scout',
+      birthDate: data.birthDate ? new Date(data.birthDate) : undefined,
+      joinDate: data.joinDate ? new Date(data.joinDate) : undefined,
+    },
+  });
+}
 
   findAll() {
-    return this.prisma.user.findMany();
-  }
+  return this.prisma.user.findMany({
+    include: {
+      section: true,
+      patrol: true,
+    },
+  });
+}
+findOne(id: string) {
+  return this.prisma.user.findUnique({
+    where: { id },
+    include: {
+      patrol: true,
+      section: true,
+    },
+  });
+}
 
   async assignSection(userId: string, sectionId: string) {
   return this.prisma.user.update({
